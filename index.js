b2bf999b173b94b1c6e35531bce3c7d1ecc816e4
irguk <igukova@gmail.com>
2022-07-24 23:21:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+function showDate() {
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  return `${day}, ${month} ${date}`;
+}
+let currentDate = document.querySelector("#date");
+currentDate.innerHTML = showDate();
+
+function showTime() {
+  let now = new Date();
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+let currentTime = document.querySelector("#time");
+currentTime.innerHTML = showTime();
+
+function showWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+function apiSearch(event) {
+  event.preventDefault();
+  let apiKey = "7095abf8ac84e0b68c9e58e564131570";
+  let city = document.querySelector("#specificSizeInputCity").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+let searchForm = document.querySelector("#search-line");
+searchForm.addEventListener("submit", apiSearch);
